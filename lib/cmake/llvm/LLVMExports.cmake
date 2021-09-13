2f1fde2044e930385b1f555278f2026cbccac30c
Renaldi_GP <39940824+Ryujinz@users.noreply.github.com>
2021-09-13 18:21:20 +0700

    Endeavour: Bump to 20210913 build
    LLVM commit: https://github.com/llvm/llvm-project/commit/1d771e19
    Clang Version: 14.0.0
    Binutils version: 2.36.1
    Builder commit: https://github.com/Ryujinz/Endeavour-Clang/commit/e28e60e41e64095473fb54f2ebdc5afe73fe428a
    
    Signed-off-by: Renaldi_GP <39940824+Ryujinz@users.noreply.github.com>

--- lib/cmake/llvm/LLVMExports.cmake
+++ lib/cmake/llvm/LLVMExports.cmake
@@ -438,14 +438,14 @@ set_target_properties(LLVMOrcJIT PROPERTIES
 add_library(LLVMOrcShared STATIC IMPORTED)
 
 set_target_properties(LLVMOrcShared PROPERTIES
-  INTERFACE_LINK_LIBRARIES "LLVMSupport"
+  INTERFACE_LINK_LIBRARIES "-lpthread;LLVMSupport"
 )
 
 # Create imported target LLVMOrcTargetProcess
 add_library(LLVMOrcTargetProcess STATIC IMPORTED)
 
 set_target_properties(LLVMOrcTargetProcess PROPERTIES
-  INTERFACE_LINK_LIBRARIES "LLVMOrcShared;LLVMSupport"
+  INTERFACE_LINK_LIBRARIES "-lpthread;LLVMOrcShared;LLVMSupport"
 )
 
 # Create imported target LLVMRuntimeDyld
